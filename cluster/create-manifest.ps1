9ebddd8a5930ec866914d408c03256fb9d0fb94c
Wes Higbee <wes.mcclure@gmail.com>
2024-04-09 19:35:50 -0500

    winget names trying v1.30.0-rc.1

--- cluster/create-manifest.ps1
+++ cluster/create-manifest.ps1
@@ -1,3 +1,9 @@
+
+# pre-requisites
+#   winget install Docker.DockerDesktop  Kubernetes.minikube  Helm.Helm
+#   winget install jqlang.jq  MikeFarah.yq  sharkdp.bat 
+#
+
 minikube --driver docker `
     --nodes 1 `
     --cpus=no-limit --memory=no-limit `
