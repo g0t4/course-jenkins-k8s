d14b560a261f4cba7d2d6358dff771b101f029a9
Wes Higbee <wes.mcclure@gmail.com>
2024-04-09 19:13:31 -0500

    cluster named manifest

---
+++ cluster/manifest.ps1
@@ -0,0 +1,7 @@
+minikube --driver docker `
+    --nodes 1 `
+    --cpus=no-limit --memory=no-limit `
+    --kubernetes-version=v1.29.3 `
+    --container-runtime=docker `
+    --profile=manifest `
+    start
