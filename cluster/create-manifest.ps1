a6cffd4d89be332d2b1992f02aceada73c5a38c3
Wes Higbee <wes.mcclure@gmail.com>
2024-04-16 17:09:51 -0500

    try 1.29.4

--- cluster/create-manifest.ps1
+++ cluster/create-manifest.ps1
@@ -7,7 +7,7 @@
 minikube --driver docker `
     --nodes 1 `
     --cpus=no-limit --memory=no-limit `
-    --kubernetes-version=v1.29.3 `
+    --kubernetes-version=v1.29.4 `
     --container-runtime=docker `
     --profile=manifest `
     start
