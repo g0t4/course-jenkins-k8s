a62674cce6196a0eb6ed7ace988cb65b2fb0962f
Wes Higbee <wes.mcclure@gmail.com>
2024-04-16 17:09:27 -0500

    try 1.30 rc2

--- cluster/create-manifest.sh
+++ cluster/create-manifest.sh
@@ -8,7 +8,7 @@
 minikube --driver docker \
     --nodes 1 \
     --cpus=no-limit --memory=no-limit \
-    --kubernetes-version=v1.30.0-rc.1 \
+    --kubernetes-version=v1.30.0-rc.2 \
     --container-runtime=docker \
     --profile=manifest \
     start
