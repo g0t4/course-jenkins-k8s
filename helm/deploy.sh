93383124db4339ebd6c1f48d8100519229292081
Wes Higbee <wes.mcclure@gmail.com>
2024-04-22 02:56:55 -0500

    update values

--- helm/deploy.sh
+++ helm/deploy.sh
@@ -3,6 +3,9 @@ helm upgrade --install -n jenkins jenkins1 jenkins/jenkins \
     --values values/additional-plugins.yaml \
     --values values/admin-pass.yaml \
     --values values/configscripts-dark-theme.yaml \
-    --values values/jenkins-url.yaml
+    --values values/jenkins-url.yaml \
+    --values values/initscripts-disable-script-security.yaml \
+    --values values/configscripts-versions-job.yaml \
+    --values values/configscripts-welcome-message.yaml
 
 # helm history -n jenkins jenkins1
