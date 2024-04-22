23c5a877bfbab0a8cf124342c2b99e54d07732da
Wes Higbee <wes.mcclure@gmail.com>
2024-04-22 02:10:34 -0500

    dark theme + jenkins URL

--- helm/deploy.sh
+++ helm/deploy.sh
@@ -1,6 +1,8 @@
 helm upgrade --install -n jenkins jenkins1 jenkins/jenkins \
     --description 'deploy.sh' \
     --values values/additional-plugins.yaml \
-    --values values/admin-pass.yaml
+    --values values/admin-pass.yaml \
+    --values values/configscripts-dark-theme.yaml \
+    --values values/jenkins-url.yaml
 
 # helm history -n jenkins jenkins1
