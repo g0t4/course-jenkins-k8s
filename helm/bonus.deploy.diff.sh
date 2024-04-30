8ecc76b21b139a05dc9f73a312f908c14d4d64ef
Wes Higbee <wes.mcclure@gmail.com>
2024-04-30 12:36:42 -0500

    notes

--- helm/bonus.deploy.diff.sh
+++ helm/bonus.deploy.diff.sh
@@ -5,6 +5,6 @@
 #    i.e. values, manifests, notes etc
 diff_two_commands 'helm get all -n jenkins jenkins1' './bonus.deploy.dry-run.sh'
 
-# my fish shell implementation of diff_two_commands:
+# you have to provide an impl of diff_two_commands, find a fish shell impl in my dotfiles repo:
 #   https://github.com/g0t4/dotfiles/blob/1823d5d26a9915c59c507c3fb19b26ae4c7640d9/fish/load_last/diff-specific.fish#L49-L60
 #   basically run both commands and pass output to icdiff command
